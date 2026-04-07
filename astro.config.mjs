ceea62a2999f8d8215d51e0fd026b256339bdf29
naufalrf4 <naufalrf4@gmail.com>
2026-04-07 12:22:20 +0700

    Add Astro project scaffolding

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+
+import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from "astro/config"
+import react from "@astrojs/react"
+import { fileURLToPath } from "url"
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+  },
+  integrations: [react()],
+})
